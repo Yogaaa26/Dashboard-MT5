efdf87ef14d310517380e5c86a967df221c14e96
Yogaaa26 <yogasptraaa26@gmail.com>
2025-07-15 18:54:18 +0700

    ganti mode penyimpanan firebase

---
+++ src/firebaseConfig.js
@@ -0,0 +1,13 @@
+// src/firebaseConfig.js
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyCdaA-CsdyY1fblY8j3082_pUk2hCivJBA",
+  authDomain: "dashboard-mt.firebaseapp.com",
+  databaseURL: "https://dashboard-mt-default-rtdb.firebaseio.com",
+  projectId: "dashboard-mt",
+  storageBucket: "dashboard-mt.firebasestorage.app",
+  messagingSenderId: "343827230016",
+  appId: "1:343827230016:web:bb25bfb07daa696f8ddea5",
+  measurementId: "G-WEDLNJ21EH"
+};
